55fe911491a8ad6943a4a0014789aef7a2ee379d
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-09 10:03:07 -0700

    Add methods #total_revenue_with_discounts and calculate_total_revenue_with_discounts

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,24 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def total_revenue_with_discounts
+    discounts_total = 0
+
+    items.joins(merchant: :discounts)
+    .select("invoice_items.item_id, MAX(invoice_items.quantity * invoice_items.unit_price * discounts.percent_discount)")
+    .where("invoice_items.quantity >= discounts.quantity")
+    .group("invoice_items.item_id").each do |row|
+      discounts_total += row.max
+    end
+
+    discounts_total
+  end
+
+  def calculate_total_revenue_with_discounts
+    total_revenue - total_revenue_with_discounts
+  end
 end
+
+
+# items.joins(merchant: :discounts).select("invoice_items.item_id, MAX(invoice_items.quantity * invoice_items.unit_price * discounts.percent_discount)").where("invoice_items.quantity >= discounts.quantity").group("invoice_items.item_id")
