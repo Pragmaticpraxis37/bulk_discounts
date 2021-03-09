7f3b12e160393cfa94b1fd0889d5cf5d7b2cc11c
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-09 12:06:44 -0700

    Modify test to conform with percent

--- spec/features/discounts/new_spec.rb
+++ spec/features/discounts/new_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe "create new discounts" do
   before :each do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     @merchant2 = Merchant.create!(name: 'Body Care')
+
+    json_response = File.read('spec/fixtures/holidays.json')
+    stub_request(:get,'https://date.nager.at/Api/v2/NextPublicHolidays/US').to_return(status: 200, body: json_response)
   end
 
   it "shows a user form to create a new bulk discount and creates it" do
-    json_response = File.read('spec/fixtures/holidays.json')
-    stub_request(:get,'https://date.nager.at/Api/v2/NextPublicHolidays/US').to_return(status: 200, body: json_response)
-
     visit new_merchant_discount_path(@merchant1)
 
     expect(page).to have_content("Create a Discount")
@@ -23,16 +23,13 @@ RSpec.describe "create new discounts" do
   end
 
   it "will not let invalid data be passed into the create form" do
-    json_response = File.read('spec/fixtures/holidays.json')
-    stub_request(:get,'https://date.nager.at/Api/v2/NextPublicHolidays/US').to_return(status: 200, body: json_response)
-
     visit new_merchant_discount_path(@merchant1)
 
     fill_in("percent_discount", with: "ten")
     fill_in("quantity", with: 10)
     click_button("Create Discount")
 
-    expect(page).to have_content("Please use only whole numbers in Percent Discount and Quantity fields")
+    expect(page).to have_content("Please use only numbers in the Percent Discount and Quantity fields")
 
     fill_in("percent_discount", with: 10)
     fill_in("quantity", with: 20)
@@ -43,14 +40,12 @@ RSpec.describe "create new discounts" do
   end
 
   it "will not let an incomplete form be accepted" do
-    json_response = File.read('spec/fixtures/holidays.json')
-    stub_request(:get,'https://date.nager.at/Api/v2/NextPublicHolidays/US').to_return(status: 200, body: json_response)
     visit new_merchant_discount_path(@merchant1)
 
     fill_in("percent_discount", with: "ten")
     click_button("Create Discount")
 
-    expect(page).to have_content("Please use only whole numbers in Percent Discount and Quantity fields")
+    expect(page).to have_content("Please use only numbers in the Percent Discount and Quantity fields")
 
     fill_in("percent_discount", with: 10)
     fill_in("quantity", with: 20)
@@ -60,14 +55,3 @@ RSpec.describe "create new discounts" do
     expect(page).to have_content("10 percent off when 20 items are bought.")
   end
 end
-
-
-
-# As a merchant
-# When I visit my bulk discounts index
-# Then I see a link to create a new discount
-# When I click this link
-# Then I am taken to a new page where I see a form to add a new bulk discount
-# When I fill in the form with valid data
-# Then I am redirected back to the bulk discount index
-# And I see my new bulk discount listed
